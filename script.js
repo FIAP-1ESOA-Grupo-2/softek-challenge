45d31679cc1ce97384126b62659a0a1ed8d96782
Gabriel Rossin Ferrante <91850393+gabrielFerrante1@users.noreply.github.com>
2024-08-20 17:39:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,70 @@
-// Constants
-const USER_MESSAGE = 'user-message'
-const BOT_MESSAGE = 'bot-message'
-const ERROR_MESSAGE = 'error-message'
-const HIDDEN = 'hidden'
+const USER_MESSAGE = 'user-message';
+const BOT_MESSAGE = 'bot-message';
+const ERROR_MESSAGE = 'error-message';
+const HIDDEN = 'hidden';
 
-// Data from the JSON file
-let softtekData
+let softtekData;
 
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        softtekData = data.softtekData
+        softtekData = data.softtekData;
     })
-    .catch(error => console.error('Error:', error))
+    .catch(error => console.error('Error:', error));
 
-const getById = id => document.getElementById(id)
+const getById = id => document.getElementById(id);
 
-// Function to toggle the sidebar
 const toggleSidebar = () => {
-    const sidebar = getById('sidebar')
-    const chatSection = getById('chatSection')
+    const sidebar = getById('sidebar');
+    const chatSection = getById('chatSection');
 
     if (sidebar.classList.contains(HIDDEN)) {
-        sidebar.classList.remove(HIDDEN)
-        chatSection.classList.add(HIDDEN)
+        sidebar.classList.remove(HIDDEN);
+        chatSection.classList.add(HIDDEN);
     } else {
-        sidebar.classList.add(HIDDEN)
-        chatSection.classList.remove(HIDDEN)
+        sidebar.classList.add(HIDDEN);
+        chatSection.classList.remove(HIDDEN);
     }
 }
-// Function to create a message element
+
 const createMessage = (type, text) => {
-    const message = document.createElement('div')
-    message.classList.add(type)
-    message.textContent = text
-    return message
+    const message = document.createElement('div');
+    message.classList.add(type);
+    message.innerHTML = text; // Use innerHTML instead of textContent
+    return message;
 }
-// Function to format the answer
+
 const formatAnswer = answer => {
     // Format bold text
-    answer = answer.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>')
+    answer = answer.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
 
     // Format italic text
-    answer = answer.replace(/\* (.*?)\ */g, '<i>$1</i>')
+    answer = answer.replace(/\* (.*?)\ */g, '<i>$1</i>');
 
     // Format inline code
-    answer = answer.replace(/`([^`]*)`/g, '<code>$1</code>')
+    answer = answer.replace(/`([^`]*)`/g, '<code>$1</code>');
 
     // Format code blocks
-    answer = answer.replace(/```([^`]*)```/g, '<pre><code>$1</code></pre>')
+    answer = answer.replace(/```([^`]*)```/g, '<pre><code>$1</code></pre>');
 
     //Format headers (h1, h2, etc.)
-    answer = answer.replace(/^### (.*$)/gim, '<h3>$1</h3>')
-    answer = answer.replace(/^## (.*$)/gim, '<h2>$1</h2>')
-    answer = answer.replace(/^# (.*$)/gim, '<h1>$1</h1>')
+    answer = answer.replace(/^### (.*$)/gim, '<h3>$1</h3>');
+    answer = answer.replace(/^## (.*$)/gim, '<h2>$1</h2>');
+    answer = answer.replace(/^# (.*$)/gim, '<h1>$1</h1>');
 
-    return answer
+    return answer;
 }
-// Function to send the user's prompt to the chatbot
+
 const sendPrompt = async () => {
-    const userInput = getById('userInput').value
-    const messageContainer = getById('messageContainer')
-    const loadingIndicator = getById('loading')
+    const userInput = getById('userInput').value;
+    const messageContainer = getById('messageContainer');
+    const loadingIndicator = getById('loading');
 
-    const userMessage = createMessage(USER_MESSAGE, userInput)
-    messageContainer.appendChild(userMessage)
+    const userMessage = createMessage(USER_MESSAGE, userInput);
+    messageContainer.appendChild(userMessage);
 
-    getById('userInput').value = ''
-    loadingIndicator.style.display = 'flex'
+    getById('userInput').value = '';
+    loadingIndicator.style.display = 'flex';
 
     try {
         const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyATsb5Oiz_kY9lAURtnVe3rMINS4wr_nFU', {
@@ -86,24 +83,24 @@ const sendPrompt = async () => {
             })
         })
 
-        const data = await response.json()
-        let answer = formatAnswer(data.candidates[0].content.parts[0].text)
+        const data = await response.json();
+        let answer = formatAnswer(data.candidates[0].content.parts[0].text);
 
-        const botMessage = createMessage(BOT_MESSAGE, answer)
-        messageContainer.appendChild(botMessage)
+        const botMessage = createMessage(BOT_MESSAGE, answer);
+        messageContainer.appendChild(botMessage);
 
         // Scroll to the bottom of the message container
-        messageContainer.scrollTop = messageContainer.scrollHeight
+        messageContainer.scrollTop = messageContainer.scrollHeight;
 
     } catch (error) {
-        const errorMessage = createMessage(BOT_MESSAGE, ERROR_MESSAGE, error.message || 'Ocorreu um erro ao se comunicar com a IA. Por favor, tente novamente mais tarde.')
-        messageContainer.appendChild(errorMessage)
+        const errorMessage = createMessage(BOT_MESSAGE, ERROR_MESSAGE, error.message || 'Ocorreu um erro ao se comunicar com a IA. Por favor, tente novamente mais tarde.');
+        messageContainer.appendChild(errorMessage);
     } finally {
-        loadingIndicator.style.display = 'none'
+        loadingIndicator.style.display = 'none';
     }
 }
-// Function to handle the user's prompt
+
 const handleClickQuestion = (problem) => {
-    getById('userInput').value = problem
-    sendPrompt()
+    getById('userInput').value = problem;
+    sendPrompt();
 }
